2f8bf3583327dcd53466185ec3e641ac6284c60e
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2024-11-02 12:08:12 -0300

    Create the Maven proyect on Spring boot with SQL
    Se crea el proyecto en spring boot y se conecta a la base de datos local para pruebas y futuros cambios-

---
+++ POLO_Proyect/src/main/java/com/theburnedrice/proyect/models/Proyecto.java
@@ -0,0 +1,8 @@
+package main.java.com.theburnedrice.proyect.models;
+
+import 
+
+@Entity
+public class Proyecto {
+
+}
---
+++ POLO_Proyect/src/main/java/com/theburnedrice/proyect/repository/Repository.java
@@ -0,0 +1,5 @@
+package main.java.com.theburnedrice.proyect.repository;
+
+public interface Repository extends JpaRepository<Proyecto, Long>{
+
+}
