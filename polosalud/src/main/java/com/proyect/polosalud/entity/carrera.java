15394f8949bb05442d67243e25e262ad0a99caf4
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2024-12-09 13:45:48 -0300

    Abduzcan

--- polosalud/src/main/java/com/proyect/polosalud/entity/carrera.java
+++ polosalud/src/main/java/com/proyect/polosalud/entity/carrera.java
@@ -24,5 +24,36 @@ public class carrera{
     @Column(nullable = false, unique = true)
     private Long codigo_carrera;
 
+    public Long getCodigo_carrera() {
+        return codigo_carrera;
+    }
+
+    public String getDescripcion_carrera() {
+        return descripcion_carrera;
+    }
+
+    public Long getId_carrera() {
+        return id_carrera;
+    }
+
+    public String getNombre_carrera() {
+        return nombre_carrera;
+    }
+
+    public void setCodigo_carrera(Long codigo_carrera) {
+        this.codigo_carrera = codigo_carrera;
+    }
+
+    public void setDescripcion_carrera(String descripcion_carrera) {
+        this.descripcion_carrera = descripcion_carrera;
+    }
+
+    public void setId_carrera(Long id_carrera) {
+        this.id_carrera = id_carrera;
+    }
+
+    public void setNombre_carrera(String nombre_carrera) {
+        this.nombre_carrera = nombre_carrera;
+    }
     
 }
